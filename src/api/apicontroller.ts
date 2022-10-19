fdc1075201f94d4afaad9a30110ea5b959e59930
Will Gunter <wgunter@udel.edu>
2022-10-19 12:32:23 -0400

    fix error

--- src/api/apicontroller.ts
+++ src/api/apicontroller.ts
@@ -38,7 +38,7 @@ export class ApiController {
         .catch(err=>res.send(err))
     }
 
-    public getEmployeePay(req: express.Request, res: express.Response): void {
+    public async getEmployeePay(req: express.Request, res: express.Response): Promise<void> {
         const employeeIDIN = req.body.employeeId;
         const startDate = req.body.startDate.toISOString();
         const endDate = req.body.endDate.toISOString();
@@ -48,12 +48,15 @@ export class ApiController {
                 const numElements = result.data.logs.length;
                 var pay = 0;
                 var time = 0;
-                result.data.logs.forEach(element => {
+                result.data.logs.forEach((element: { clock_in_date_time: number; clock_out_date_time: number; pay: number; time: number; }) => {
                     if (element.clock_in_date_time >= startDate && element.clock_out_date_time <= endDate) {
                         pay += element.pay;
                         time += element.time;
                     }
-                    
+                });
+                res.send({pay: pay, time: time});
+        } catch (error) {
+            res.send(error);
         }
     }
 }
