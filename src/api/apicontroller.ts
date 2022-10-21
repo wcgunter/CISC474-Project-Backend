8049378a4661c7906cc517b7f7c5ee27dd253b54
Robert Sammataro <robbysam@udel.edu>
2022-10-21 15:07:10 -0400

    Fixed crashing on invalid URL

--- src/api/apicontroller.ts
+++ src/api/apicontroller.ts
@@ -16,6 +16,40 @@ export class ApiController {
         });
     }
 
+    //Lookup by a user ID (Currently defaults to 00000000)
+    public async getUserLog(req: express.Request, res: express.Response): Promise<void> {
+        let db = await MongoDb.client.connect(); //connect to mongo
+        let dbo = db.db(MongoDb.database); //get our database
+        
+        dbo.collection("employees").findOne({"employee_id": req.params.id}, (err:any, result:any) => {
+            if (err) throw err;
+            
+            try{
+                res.status(200).send(result.logs);
+                console.log(result.logs);
+            } catch {
+                res.status(200).send(result);
+                console.log(result);
+            }
+            
+            db.close();
+        });
+    }
+
+    //Get all users for a specific manager id
+    public async queryByManagerId(req: express.Request, res: express.Response): Promise<void> {
+        let db = await MongoDb.client.connect(); //connect to mongo
+        let dbo = db.db(MongoDb.database); //get our database
+        
+        dbo.collection("employees").find({"manager_id": req.params.id}).toArray((err:any, result:any) => {
+            if (err) throw err;
+            console.log(result);
+            res.status(200).send(result);
+            db.close();
+          });
+        
+    }
+
 
     public postHello(req: express.Request, res: express.Response): void {
 
