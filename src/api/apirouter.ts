84f2fbebfb33d74a89e22d0b2166b1c5289d29bf
Haritima23 <haritima@udel.edu>
2022-10-21 20:04:42 -0400

    Clock Events End Points Added - POST/PUT/DELETE

--- src/api/apirouter.ts
+++ src/api/apirouter.ts
@@ -9,6 +9,9 @@ export class ApiRouter {
     public getRouter(): express.Router {
         this.router.get("/hello", this.controller.getHello);
         this.router.get("/employees/:employeeID_in/:startDate_in/:endDate_in", this.controller.getEmployeesPay);
+        this.router.post("/employees/:employeeID_in/:clock_in/:clock_out", this.controller.addClockEvent);
+        this.router.delete("/employees/:employeeID_in/:clock_in/:clock_out", this.controller.deleteClockEvent);
+        this.router.put("/employees/:employeeID_in/:original_clock_in/:original_clock_out/:new_clock_in/:new_clock_out", this.controller.replaceClockEvent);
         return this.router;
     }
 }
