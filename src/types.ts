0b3af267490f3dbe6c4ccd4c9fc76623103e8748
Benjamin Raymon <braymon@udel.edu>
2022-11-04 11:54:39 -0400

    added Employee type

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+import { Double } from "mongodb"
+
+export interface Employee {
+	first_name: String,
+	last_name: String,
+	address: String,
+	employee_id?: String,
+	jobs: [{
+			start_date: Date,
+			title: String,
+			pay_rate: Double,
+			level: String
+	}],
+	manager_id: String,
+	status: String,
+	logs?: [{
+		clock_in_date_time: Date,
+		clock_out_date_time: Date,
+		clock_id: Number
+	}] 
+}
